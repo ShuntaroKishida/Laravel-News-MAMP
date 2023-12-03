2990bc989eaa0a0416f2c7635b0ca7b07b2cb8dd
ShuntaroKishida <137754393+ShuntaroKishida@users.noreply.github.com>
2023-12-03 17:12:55 +0900

    first commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::all();
+        return view('post.index', compact('posts'));
+    }
+
+    public function create()
+    {
+        return view('post.index');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|max:30',
+            'body' => 'required',
+        ]);
+        $post = Post::create($validated);
+        return back();
+    }
+
+    public function show(Post $post)
+    {
+        return view('post.show', compact('post'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
@@ -30,14 +29,4 @@ Route::prefix('post')->group(function () {
     Route::delete('{post}/comment/{comment}', [CommentController::class, 'destroy'])->name('comment.destroy');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
+?>
